630d6d9afe17ccddc8a56166e21517d4bf03077f
Joshkebab <joshkebab32@gmail.com>
2025-01-16 23:01:38 +0000

    Animated more topbar stuff
    it expands when needed and hides the below-contacts when not needed.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const topbar = document.querySelector('.topbar');
+
+// Toggle contacts on mobile/small screens
 function toggleContacts() {
-    const topbar = document.querySelector('.topbar');
     topbar.classList.toggle('show-contacts');
 }
+
+// Handle the resizing of the window and close the contacts if the window is resized to a larger size
+function handleResize() {
+    const belowContacts = document.querySelector('.below-contacts');
+    if (window.innerWidth >= 1000) {
+        if (belowContacts.style.display === 'flex') {
+            topbar.classList.remove('show-contacts');
+        }
+        belowContacts.style.display = 'none';
+    } else {
+        belowContacts.style.display = 'flex';
+    }
+}
+
+window.addEventListener('resize', handleResize);
+window.addEventListener('load', handleResize);
